4909d7dadd85279a5867e692a54d295950a87d53
Omkar Arora <aroraomkar@gmail.com>
2021-10-04 13:33:09 +0530

    feat: Pick random cards

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { cards } from "./data";
 
 function App() {
   const [cardsData, setCardsdata] = useState(cards);
+  const [pickedCards, setPickedCards] = useState(null);
 
   function shuffleCards() {
     let shuffled = cardsData
@@ -12,6 +13,16 @@ function App() {
       .sort((a, b) => a.sort - b.sort)
       .map(({ value }) => value);
     setCardsdata(shuffled);
+    setPickedCards(null);
+  }
+
+  function pickCards(numOfElements) {
+    let selectedCards = [];
+    for (let i = 0; i < numOfElements; i++) {
+      let element = cardsData[Math.floor(Math.random() * cardsData.length)];
+      selectedCards.push(element);
+    }
+    setPickedCards(selectedCards);
   }
 
   return (
@@ -25,7 +36,14 @@ function App() {
         <button className="btn-action" onClick={shuffleCards}>
           Shuffle
         </button>
-        <button className="btn-action">Pick 5 Cards</button>
+        <button className="btn-action" onClick={() => pickCards(5)}>
+          Pick 5 Cards
+        </button>
+      </div>
+      <div className="container-pickedCards">
+        {pickedCards &&
+          pickedCards.length > 0 &&
+          pickedCards.map((card) => <PlayCard {...card} />)}
       </div>
     </div>
   );
